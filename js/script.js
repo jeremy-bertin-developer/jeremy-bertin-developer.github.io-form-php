1de72002188017bcb3cda3be8a500dc7976e67e9
Jeremy Bertin <jeremy.bertin76@hotmail.fr>
2020-12-02 18:52:22 +0100

    second commit

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $(function () {
 
         $.ajax({
             type: 'POST',
-            url: 'php/contact.php',
+            url: '../php/contact.php',
             data: postdata,
             dataType: 'json',
             success: function(json) {
